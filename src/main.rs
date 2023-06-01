b628ee9cd7391ae1ea09e0ec8f0c7ccbc56a2f49
stephen <stephen.buchanan@random.studio>
2023-06-01 09:31:18 +0200

    demo no-gui mode

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
+use clap::{command, Parser};
 use eframe::egui;
+use env_logger::Env;
+use log::info;
 
-fn main() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "My egui App",
-        options,
-        Box::new(|_cc| Box::<MyApp>::default()),
-    )
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[arg(long = "loglevel",default_value_t=String::from("info"))]
+    log_level: String,
+
+    #[arg(long = "headless")]
+    headless_mode: bool,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    env_logger::Builder::from_env(Env::default().default_filter_or(&cli.log_level)).init();
+
+    if cli.headless_mode {
+        info!("Running in headless mode; no graphic output");
+    } else {
+        let options = eframe::NativeOptions {
+            initial_window_size: Some(egui::vec2(320.0, 240.0)),
+            ..Default::default()
+        };
+        eframe::run_native(
+            "My egui App",
+            options,
+            Box::new(|_cc| Box::<MyApp>::default()),
+        )
+        .expect("Failed to launch GUI");
+    }
 }
 
 struct MyApp {
@@ -26,7 +46,6 @@ impl Default for MyApp {
         }
     }
 }
-
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
