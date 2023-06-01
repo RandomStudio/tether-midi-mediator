3f64ce68715f9f161dceeb8a340acc328eccafe0
stephen <stephen.buchanan@random.studio>
2023-06-01 10:38:10 +0200

    ui and midi on separate threads

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::{
     error::Error,
     io::{stdin, stdout, Write},
+    sync::mpsc::{self, Receiver},
+    time::Duration,
 };
 
 use clap::{command, Parser};
 use eframe::egui;
 use env_logger::Env;
-use log::{debug, info, warn};
+use log::{debug, error, info, warn};
 use midi_msg::{ControlChange, MidiMsg, ReceiverContext, SystemRealTimeMsg};
 use midir::{Ignore, MidiInput};
 
@@ -27,28 +29,43 @@ fn main() {
     let cli = Cli::parse();
     env_logger::Builder::from_env(Env::default().default_filter_or(&cli.log_level)).init();
 
-    match get_midi_input(cli.midi_port) {
+    let mut handles = Vec::new();
+
+    let (tx, rx) = mpsc::channel();
+
+    let midi_tx = tx.clone();
+    let midi_thread = std::thread::spawn(move || match get_midi_input(cli.midi_port, midi_tx) {
         Ok(_) => (),
         Err(err) => println!("Error: {}", err),
-    }
+    });
+    handles.push(midi_thread);
 
     if cli.headless_mode {
         info!("Running in headless mode; no graphic output");
+        for received in rx {
+            println!("Received MIDI message: {:?}", received);
+            // std::thread::sleep(Duration::from_millis(1));
+        }
     } else {
+        info!("Running graphics mode");
         let options = eframe::NativeOptions {
-            initial_window_size: Some(egui::vec2(320.0, 240.0)),
+            // initial_window_size: Some(egui::vec2(320.0, 240.0)),
             ..Default::default()
         };
         eframe::run_native(
             "My egui App",
             options,
-            Box::new(|_cc| Box::<MyApp>::default()),
+            Box::new(|_cc| Box::<Model>::new(Model::new(rx))),
         )
         .expect("Failed to launch GUI");
     }
+
+    for handle in handles {
+        handle.join().expect("failed to join thread handle");
+    }
 }
 
-fn get_midi_input(preferred_port: usize) -> Result<(), Box<dyn Error>> {
+fn get_midi_input(preferred_port: usize, tx: mpsc::Sender<MidiMsg>) -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
 
     let mut midi_in = MidiInput::new("midir reading input")?;
@@ -93,6 +110,8 @@ fn get_midi_input(preferred_port: usize) -> Result<(), Box<dyn Error>> {
             let (msg, _len) =
                 MidiMsg::from_midi_with_context(&midi_bytes, &mut ctx).expect("Not an error");
 
+            tx.send(msg);
+
             // // Print everything but spammy clock messages.
             // if let MidiMsg::SystemRealTime {
             //     msg: SystemRealTimeMsg::TimingClock,
@@ -102,36 +121,37 @@ fn get_midi_input(preferred_port: usize) -> Result<(), Box<dyn Error>> {
             // } else {
             //     println!("{}: {:?}", stamp, msg);
             // }
-            match msg {
-                MidiMsg::ChannelVoice { channel, msg } => {
-                    println!("Channel {:?}, msg: {:?}", channel, msg);
-                    match msg {
-                        midi_msg::ChannelVoiceMsg::NoteOn { note, velocity } => {
-                            println!("NoteOn {}, @ {}", note, velocity);
-                        }
-                        midi_msg::ChannelVoiceMsg::NoteOff { note, velocity } => {
-                            println!("NoteOff {}, @ {}", note, velocity);
-                        }
-                        midi_msg::ChannelVoiceMsg::ControlChange { control } => {
-                            println!("ControlChange message: {:?}", control);
-                            match control {
-                                ControlChange::Undefined { control, value } => {
-                                    println!("'Undefined' control change message: control = {control}, value = {value}");
-                                },
-                                _ => {
-                                    warn!("This type of ControlChange message not handled (yet)")
-                                }
-                            }
-                        }
-                        _ => {
-                            warn!("This type of ChannelVoiceMessage not handled (yet)");
-                        }
-                    }
-                }
-                _ => {
-                    debug!("unhandled midi message: {:?}", msg);
-                }
-            }
+
+            // match msg {
+            //     MidiMsg::ChannelVoice { channel, msg } => {
+            //         println!("Channel {:?}, msg: {:?}", channel, msg);
+            //         match msg {
+            //             midi_msg::ChannelVoiceMsg::NoteOn { note, velocity } => {
+            //                 println!("NoteOn {}, @ {}", note, velocity);
+            //             }
+            //             midi_msg::ChannelVoiceMsg::NoteOff { note, velocity } => {
+            //                 println!("NoteOff {}, @ {}", note, velocity);
+            //             }
+            //             midi_msg::ChannelVoiceMsg::ControlChange { control } => {
+            //                 println!("ControlChange message: {:?}", control);
+            //                 match control {
+            //                     ControlChange::Undefined { control, value } => {
+            //                         println!("'Undefined' control change message: control = {control}, value = {value}");
+            //                     },
+            //                     _ => {
+            //                         warn!("This type of ControlChange message not handled (yet)")
+            //                     }
+            //                 }
+            //             }
+            //             _ => {
+            //                 warn!("This type of ChannelVoiceMessage not handled (yet)");
+            //             }
+            //         }
+            //     }
+            //     _ => {
+            //         debug!("unhandled midi message: {:?}", msg);
+            //     }
+            // }
         },
         (),
     )?;
@@ -148,33 +168,34 @@ fn get_midi_input(preferred_port: usize) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-struct MyApp {
-    name: String,
-    age: u32,
+struct Model {
+    last_msg_received: String,
+    rx: Receiver<MidiMsg>,
 }
 
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
+impl Model {
+    pub fn new(rx: Receiver<MidiMsg>) -> Self {
+        Model {
+            rx,
+            last_msg_received: "".to_owned(),
         }
     }
 }
-impl eframe::App for MyApp {
+
+impl eframe::App for Model {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.request_repaint();
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+            ui.heading("Tether Midi Mediator");
+            ui.label("Last message received:");
+            ui.small(&self.last_msg_received);
         });
+
+        if let Ok(msg) = &self.rx.try_recv() {
+            println!("GUI received MIDI message: {:?}", msg);
+            self.last_msg_received = format!("{:?}", msg);
+            std::thread::sleep(Duration::from_millis(1));
+        }
     }
 }
